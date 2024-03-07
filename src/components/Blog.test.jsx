8329237562ae28e9d68f05399ef9b9389ec48c39
Sanvals <santano.sergio@gmail.com>
2024-03-07 14:08:11 +0100

    Blog List Tests, step 2

--- src/components/Blog.test.jsx
+++ src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 
@@ -32,4 +33,36 @@ test('renders content', () => {
 
   const author = screen.getAllByText(/Josefa/)
   expect(author).toBeDefined()
+})
+
+test('shows number of likes', () => {
+  const blog =
+    {
+      title: 'Component testing',
+      url: '',
+      likes: 21,
+      author: {
+        username: 'Josefa',
+        name: 'Josefina',
+        id: '65e1c334831732d222a6151c'
+      },
+      likedBy: ['Josefa'],
+      id: '65e4aecc9cd77c43b4fd1053'
+    }
+
+  render(
+    <Blog
+      blog={blog}
+      user='Josefa'
+      deleteBlogApp={vi.fn()}
+    />
+  )
+
+  const user = userEvent.setup()
+  const button = screen.getByText('❤️')
+  user.click(button)
+
+  const likes = screen.getByText(/21/)
+  // screen.debug()
+  expect(likes).toBeDefined()
 })
